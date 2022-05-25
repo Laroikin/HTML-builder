ca964f28880d8af74631a430ae664c0210d753c9
Laroikin <said24abdi04@gmail.com>
2022-05-25 20:56:19 +0900

    refactor: implement chunk reading right (среда, 25 мая 2022 г. 20:56:19 (KST))

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const fs = require('fs');
-const process = require('process');
+const { stdout } = require('process');
 
-const textFile = fs.createReadStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
+const textFile = fs.createReadStream(
+  path.resolve(__dirname, 'text.txt'),
+  'utf-8'
+);
 
-textFile.on('data', data => process.stdout.write(data));
+let textData = '';
+
+textFile.on('data', data => (textData += data));
+textFile.on('end', () => stdout.write(textData));
