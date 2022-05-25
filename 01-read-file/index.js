a4b78e5fa844971d1e0a88ed1b40ab1a8e580651
Laroikin <said24abdi04@gmail.com>
2022-05-25 20:51:17 +0900

    refactor: use 'utf-8' argument on createReadStream instead of toString method среда, 25 мая 2022 г. 20:51:17 (KST)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const process = require('process');
 
-const textFile = fs.createReadStream(path.resolve(__dirname, 'text.txt'));
+const textFile = fs.createReadStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
 
-textFile.on('data', data => process.stdout.write(data.toString()));
+textFile.on('data', data => process.stdout.write(data));
