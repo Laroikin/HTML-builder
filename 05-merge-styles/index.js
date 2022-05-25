42fb8dab3369c1951fd801ffa484ebaf0b61f2b3
Laroikin <said24abdi04@gmail.com>
2022-05-26 02:18:12 +0900

    feat: solve 05-merge-styles (четверг, 26 мая 2022 г. 02:18:12 (KST))

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { access, unlink } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -6,6 +6,13 @@ const __dirname = path.dirname(__filename);
 
 fs.readdir(path.resolve(__dirname, 'styles'), (err, files) => {
   if (err) throw err;
+
+  const tempStream = fs.createWriteStream(
+    path.resolve(__dirname, 'project-dist', 'bundle.css')
+  );
+
+  tempStream.write('');
+
   for (let i = 0; i < files.length; i++) {
     const pathToFile = path.resolve(__dirname, 'styles', files[i]);
     if (path.extname(pathToFile) === '.css') {
