bcb577afb6404ee1da6bb0f8437b0f89dcd343a6
Laroikin <said24abdi04@gmail.com>
2022-05-26 02:37:44 +0900

    refactor: small refactor (четверг, 26 мая 2022 г. 02:37:44 (KST))

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,13 +7,15 @@ const __dirname = path.dirname(__filename);
 async function copyDir() {
   fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
     if (err) throw err;
+
     fs.readdir(path.join(__dirname, 'files-copy'), (_err, copy) => {
-      for (let j = 0; j < copy.length; j++) {
-        fs.unlink(path.join(__dirname, 'files-copy', copy[j]), err => {
+      for (let i = 0; i < copy.length; i++) {
+        fs.unlink(path.join(__dirname, 'files-copy', copy[i]), err => {
           if (err) throw err;
         });
       }
     });
+
     fs.readdir(path.join(__dirname, 'files'), (_err, items) => {
       for (let i = 0; i < items.length; i++) {
         fs.copyFile(
