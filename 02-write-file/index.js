c098399d488c36da4971a683a55d7a4d5c7a8145
Laroikin <said24abdi04@gmail.com>
2022-05-27 22:33:50 +0900

    refactor: replace default stdin to readline (пятница, 27 мая 2022 г. 22:33:50 (KST))

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,34 +1,23 @@
-import process from 'process';
-import { createWriteStream } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import * as readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
+import { createReadStream, createWriteStream } from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 console.log(
   'Hello! Please write down the things you want to include in the file and enter exit or press Ctrl+C to end the program: '
 );
 
-function dataHandler(data) {
-  data = data.toString();
-  if (data.trim() == 'exit') {
-    process.exit();
-  }
-
-  writeStream.write(data);
-}
-
-const writeStream = createWriteStream(path.resolve(__dirname, 'output.txt'), {
-  flags: 'a',
-  encoding: 'utf-8'
+const rl = readline.createInterface({ input, output });
+const writeFile = createWriteStream(path.resolve(__dirname, 'output.txt'), {
+  flags: 'a'
 });
 
-process.stdin.on('data', dataHandler);
-
-process.on('SIGINT', function () {
-  process.exit();
+rl.on('line', line => {
+  if (line == 'exit') rl.close();
+  else writeFile.write(line + '\n');
 });
 
-process.on('exit', () => console.log('Bye!'));
-
-writeStream.on('error', error => console.log(error));
+rl.on('close', () => console.log('Bye'));
